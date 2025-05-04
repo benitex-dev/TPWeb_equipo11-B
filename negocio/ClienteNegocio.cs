57717e2c13364aa8b41df0d721841b4dfa2af687
benitex-dev <benitezjorgedev@gmail.com>
2025-05-04 20:00:01 -0300

    Se agrega clase ClienteNegocio con metodo agregarCliente()

---
+++ negocio/ClienteNegocio.cs
@@ -0,0 +1,42 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class ClienteNegocio
+    {
+        //metodo para agregar un cliente a nuestra DB
+        public void agregarCliente(Cliente cliente) { 
+         AccesoDatos accesoDatos = new AccesoDatos();
+            try
+            {
+                accesoDatos.setConsulta("INSERT INTO Clientes (Documento,Nombre,Apellido,Email,Direccion,Ciudad,CP)\r\n  " +
+                                        "VALUES\r\n  " +
+                                        "(@dni,@nombre,@apellido,@email,@direccion,@ciudad,@cp)");
+                accesoDatos.setParametro("@dni",cliente.Dni);
+                accesoDatos.setParametro("@nombre",cliente.Nombre);
+                accesoDatos.setParametro("@apellido",cliente.Apellido);
+                accesoDatos.setParametro("@email",cliente.Email);
+                accesoDatos.setParametro("@direccion",cliente.Direccion);
+                accesoDatos.setParametro("@ciudad",cliente.Ciudad);
+                accesoDatos.setParametro("@cp",cliente.CodPostal);
+
+                accesoDatos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+        }
+
+    }
+}
