16476c31ca9fc70084310de4477f2f53a28087fc
Ivan Baigorria <ivann.baigorria@gmail.com>
2025-05-04 21:21:40 -0300

    Se agrega la clase Voucher en Dominio

---
+++ dominio/Voucher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Voucher
+    {
+        [DisplayName("Código Voucher")]
+        public string CodigoVoucher {  get; set; }
+        public int IdCliente { get; set; }
+        public DateTime FechaCanje { get; set; }
+        public int IdArticulo { get; set; }
+    }
+}
